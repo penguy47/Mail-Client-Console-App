2ab645a66c8b688e218afe780d8a53785590e52e
MrPen <lan474444@gmail.com>
2024-09-14 10:35:05 +0700

    add bin

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,6 @@ package src;
 import src.config.*;
 public class Main {
     public static void main(String[] args) {
-        new EmailController(XMLConfigReader.readConfig("src\\config.xml"));
+        new EmailController(XMLConfigReader.readConfig("config.xml"));
     }
 }
